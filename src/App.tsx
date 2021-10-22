8fbfa64867ddb1442eeec23a15a43a4bab177030
ka2jun8 <ka210n8@gmail.com>
2021-10-22 10:03:35 +0900

    viewValues

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,50 @@ import { Link, Route, Router, Switch } from "wouter";
 
 const About = lazy(() => import("./About"));
 
-export const App: React.VFC = () => (
-  <Router>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-      </ul>
-    </nav>
+declare global {
+  interface Window {
+    viewValues: {sample: string};
+  }
+}
 
-    {/* A <Switch> looks through its children <Route>s and
-        renders the first one that matches the current URL. */}
-    <Switch>
-      <Route path="/about">
-        <React.Suspense fallback="loading...">
-          <About />
-        </React.Suspense>
-      </Route>
-      <Route path="/users">
-        <Users />
-      </Route>
-      <Route path="/">
-        <Home />
-      </Route>
-    </Switch>
-  </div>
-</Router>
-)
+export const App: React.VFC = () => {
+  return (
+    <Router>
+    <div>
+      {window.viewValues.sample}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+          <React.Suspense fallback="loading...">
+            <About />
+          </React.Suspense>
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+  )
+}
 
 function Home() {
   return <h2>Home</h2>;
